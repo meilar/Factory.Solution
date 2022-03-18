08fd42c54c3bf4ebd7abab66df625306c19e0ccb
Matt Eilar <meilar@gmail.com>
2022-03-18 11:17:11 -0700

    WIP: adding create form

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -10,7 +10,7 @@ namespace Factory.Models
     }
 
     public int MachineId { get; set; }
-    public string ModelName { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
