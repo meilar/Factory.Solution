d5f8b78e32e1dc2d34cc2e64415bcb82d0cac5ff
Matt Eilar <meilar@gmail.com>
2022-03-19 19:11:41 -0700

    add delete function

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -57,5 +57,18 @@ namespace Factory.Controllers
         .FirstOrDefault(machine => machine.MachineId == id);
       return View(thisMachine);
     }
+    public ActionResult Delete(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
     }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      _db.Machines.Remove(thisMachine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
