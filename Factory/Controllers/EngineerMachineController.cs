d8323caec77342f49e155c903007181f99960ea4
Matt Eilar <meilar@gmail.com>
2022-03-19 19:24:22 -0700

    add edit function

---
+++ Factory/Controllers/EngineerMachineController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+    public class EngineerMachineController : Controller
+    {
+      private readonly FactoryContext _db;
+      public EngineerMachineController(FactoryContext db)
+      {
+        _db = db;
+      }
+      [HttpPost]
+      public ActionResult Edit(int EngineerId, int MachineId)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = MachineId });
+        _db.SaveChanges();
+        return View();
+      }
+    }
+}
